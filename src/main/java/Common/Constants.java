59cf65a214daacd217b16e6328f34cf93ec8b0e8
Cathy Qin <qin.xuj@northeastern.edu>
2024-04-01 18:55:14 -0700

    package refactor

--- src/main/java/Common/Constants.java
+++ src/main/java/Common/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
   public final static String TXT_FILE_PATH_PATTERN = "([A-Z]:)?([\\.\\\\/]?[A-Za-z0-9_-]*[\\.\\\\/])*[A-Za-z0-9_-]*\\.txt";
   public final static String CSV_FILE_PATH_PATTERN = "([A-Z]:)?([\\.\\\\/]?[A-Za-z0-9_-]*[\\.\\\\/])*[A-Za-z0-9_-]*\\.csv";
   public final static String OUTPUT_DIR_PATTERN = "([A-Z]:)?([\\.\\\\/]?[A-Za-z0-9_-]*[\\.\\\\/])*";
-  public static final String DEFAULT_ERROR_MESSAGE = "\nUsage：" +
+  public final static String DEFAULT_ERROR_MESSAGE = "\nUsage：" +
       "\n--email Generate email messages. If this option is provided, then --email-template must also be provided." +
       "\n--email-template <path/to/file> A filename for the email template. --letter Generate letters. If this option is provided, then --letter-template must also be provided." +
       "\n--letter-template <path/to/file> A filename for the letter template. --output-dir <path/to/folder> The folder to store all generated files. This option is required." +
